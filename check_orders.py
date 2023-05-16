b7d88751e4eca1fcbfe95568e346d68728bf007c
Wambui-Mwangi <124131028+Wambui-Mwangi@users.noreply.github.com>
2023-05-16 15:18:51 +0300

    Update check_orders.py

--- check_orders.py
+++ check_orders.py
@@ -1,7 +1,7 @@
 class CheckOrders:
-    def __init__(self,customerName):
+    def __init__(self,customer_name):
         self.orders = []
-        self.customerName=customerName
+        self.customerName=customer_name
 #  adds the new orders to the orders list
     def add_order(self, order):
         self.orders.append(order)
@@ -11,34 +11,34 @@ class CheckOrders:
 # you can get orders from the list
     def get_orders(self):
         return self.orders
-    def get_order_by_id(self, Name):
+    def get_order_by_id(self, name):
         for order in self.orders:
-            if Name == Name:
+            if name == name:
                 return order
 # used to represent a single order
 class Orders:
-    def __init__(self,Name,order,payment,deliveryAdress,status) :
+    def __init__(self,name,order,payment,deliveryAdress,status) :
         self.order=order
-        self.Name=Name
+        self.name=name
         self.payment=payment
         self.deliveryAddress=deliveryAdress
         self.status=status
         #  returns a dictionary containing the details
     def add_order(self):
         orders_dict={
-           "name":self.Name,
+           "name":self.name,
            "order":self.order,
            "payment":self.payment,
            "deliveryAdress":self.deliveryAddress,
            "status":self.status
        }
         return orders_dict
-Name=input("input your name")
+name=input("input your name")
 order=input("Input the type of order")
 payment=input("Input the type of payment")
 deliveryAddress=input("Input your delivery address")
 status=input("Add status of your order")
-new_Customer=Orders(Name,order,payment,deliveryAddress,status)
+new_Customer=Orders(name,order,payment,deliveryAddress,status)
 user=new_Customer
 print(new_Customer.add_order())
 my_orders = CheckOrders("John Doe")
@@ -46,5 +46,5 @@ new_order = Orders("Jill", "Product A", 10.99,"JNFJ","LDJNF")
 print(my_orders.add_order(new_order))
 all_orders = my_orders.get_orders()
 # all orders are retrieved using the get_order_id method
-Name = "Jill"
-order = my_orders.get_order_by_id(Name)
+name = "Jill"
+order = my_orders.get_order_by_id(name)
